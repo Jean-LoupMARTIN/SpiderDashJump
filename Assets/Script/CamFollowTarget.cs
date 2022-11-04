7c8bee9c7ab78cf65ce17206f39d932b1039ec4d
Jean-LoupMARTIN <jlmc78@outlook.fr>
2022-11-04 23:50:53 +0100

    Import

---
+++ Assets/Script/CamFollowTarget.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+
+[DefaultExecutionOrder(3)]
+public class CamFollowTarget : MonoBehaviour
+{
+    static public CamFollowTarget inst;
+
+    [SerializeField] Transform target;
+    [SerializeField] float positionSpeed = 10;
+    [SerializeField] float rotationSpeed = 10;
+    [SerializeField] LayerMask layer;
+
+    Vector3 posProj;
+    Quaternion rotProj;
+
+    Player3D player3D;
+    Controller controller;
+    [SerializeField] float stickAngle = 30;
+
+    public Player3D Player3D { get => player3D; }
+
+    void Awake()
+    {
+        inst = this;
+
+        posProj = target.InverseTransformPoint(transform.position);
+        rotProj = Quaternion.Inverse(target.rotation) * transform.rotation;
+
+        player3D = target?.GetComponentInParent<Player3D>();
+        controller = player3D?.Controller;
+    }
+
+    void Update()
+    {
+        Quaternion stickRot = Quaternion.Euler(-stickAngle * controller.StickArcValue(false, false, 60), 0, 0);
+
+        Quaternion rot = target.rotation * rotProj * stickRot;
+
+        Vector3 pos = target.TransformPoint(stickRot * posProj);
+        Vector3 dir = pos - target.position;
+
+        if (Physics.Raycast(target.position, dir, out RaycastHit hit, dir.magnitude, layer))
+            pos = hit.point;
+
+        transform.position = Vector3   .Lerp(transform.position, pos, Time.deltaTime * positionSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ Assets/Script/Extension/PostProcessingExtension.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Volume))]
+public class PostProcessingExtension : MonoBehaviour
+{
+    static PostProcessingExtension inst;
+
+    Volume volume;
+    ChromaticAberration chromaticAberration;
+    LensDistortion lensDistortion;
+
+    void Awake()
+    {
+        inst = this;
+        Cache();
+    }
+
+
+    void Cache()
+    {
+        volume = GetComponent<Volume>();
+        volume.profile.TryGet(out chromaticAberration);
+        volume.profile.TryGet(out lensDistortion);
+    }
+
+    public static void StartChromaticAberrationCoroutine(float addIntensity, float fadeInTime, float fadeOutTime)
+    {
+        if (inst && inst.chromaticAberration)
+            inst.StartCoroutine(inst.PostProCoroutine(addIntensity, fadeInTime, fadeOutTime, x => inst.chromaticAberration.intensity.value += x));
+    }
+
+    public static void StartLensDistortionCoroutine(float addIntensity, float fadeInTime, float fadeOutTime)
+    {
+        if (inst && inst.lensDistortion)
+            inst.StartCoroutine(inst.PostProCoroutine(addIntensity, fadeInTime, fadeOutTime, x => inst.lensDistortion.intensity.value += x));
+    }
+
+    IEnumerator PostProCoroutine(float intensity, float fadeInTime, float fadeOutTime, Action<float> AddItensity)
+    {
+        float t = 0;
+        float progress;
+
+        float lastIntensity = 0;
+        float nextIntensity;
+        float dIntensity;
+
+        while (t < fadeInTime)
+        {
+            t += Time.deltaTime;
+            progress = Mathf.Clamp01(t / fadeInTime);
+
+            nextIntensity = intensity * progress;
+            dIntensity = nextIntensity - lastIntensity;
+            lastIntensity = nextIntensity;
+
+            AddItensity(dIntensity);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        t = 0;
+
+        while (t < fadeOutTime)
+        {
+            t += Time.deltaTime;
+            progress = Mathf.Clamp01(t / fadeOutTime);
+
+            nextIntensity = intensity * (1 - progress);
+            dIntensity = nextIntensity - lastIntensity;
+            lastIntensity = nextIntensity;
+
+            chromaticAberration.intensity.value += dIntensity;
+            AddItensity(dIntensity);
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
